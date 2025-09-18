8583b74ed393af1ff4fdba15cfa9c1f938af2e8a
Dikadwii <kikidos3@gmail.com>
2025-09-18 17:53:53 +0700

    update

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def getWelcome():#function untuk menghandle endpoint diatas
     }
 
 #endpoint untuk menampilkan semua dataset
-@app.get("/DIKA")
+@app.get("/data")
 def GetData():
     #melakukan proses pengambilan data dari csv
     df = pd.read_csv("dataset.csv")
